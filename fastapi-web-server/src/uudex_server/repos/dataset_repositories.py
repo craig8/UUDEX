21b474d7a17627bc0bee1958622bacc5dfe624ec
C. Allwardt <3979063+craig8@users.noreply.github.com>
2025-08-18 09:34:02 -0700

    fix: Refactor import statements for Dataset and DatasetDefinition in dataset_repositories.py

--- fastapi-web-server/src/uudex_server/repos/dataset_repositories.py
+++ fastapi-web-server/src/uudex_server/repos/dataset_repositories.py
@@ -1,23 +1,22 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import select as _select
 
-import uudex_server.models as m
-from uudex_server.models import Dataset
+from uudex_server.models import Dataset, DatasetDefinition
 from uudex_server.repos import Repository
 
 
 class DatasetRepository(Repository[Dataset]):
     def __init__(self, session: AsyncSession):
-        super().__init__(m.Dataset, session=session, id_field="dataset_id")
+        super().__init__(Dataset, session=session, id_field="dataset_id")
 
     async def select_datasets_by_participant(
         self, session: AsyncSession, participant_id: int
-    ) -> list[m.Dataset]:
-        statement = _select(m.Dataset).where(Dataset.owner_participant_id == participant_id)
+    ) -> list[Dataset]:
+        statement = _select(Dataset).where(Dataset.owner_participant_id == participant_id)
         res = await session.execute(statement)
         return list(res.scalars().all())
 
 
-class DatasetDefinitionRepository(Repository[m.DatasetDefinition]):
+class DatasetDefinitionRepository(Repository[DatasetDefinition]):
     def __init__(self, session: AsyncSession):
-        super().__init__(m.DatasetDefinition, session=session, id_field="dataset_definition_id")
+        super().__init__(DatasetDefinition, session=session, id_field="dataset_definition_id")
